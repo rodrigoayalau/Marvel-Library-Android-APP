ed16f7238bd3eb6d63b368206a3339c28a3d3217
Rodrigo Ayala <ids.ayala@gmail.com>
2019-05-14 23:24:41 -0500

    android changes

--- app/src/main/java/com/example/marvellibrary/Deserealizer.java
+++ app/src/main/java/com/example/marvellibrary/Deserealizer.java
@@ -17,7 +17,7 @@ public class Deserealizer implements JsonDeserializer<MarvelModelDeserealizer> {
         String copyright = json.getAsJsonObject().get("copyright").getAsString();
         int total = json.getAsJsonObject().get("data").getAsJsonObject().get("total").getAsInt();
         String name = json.getAsJsonObject().get("data").getAsJsonObject().get("results").getAsJsonArray().get(0).getAsJsonObject().get("name").getAsString();
-        
+
         MarvelModelDeserealizer marvel = new MarvelModelDeserealizer(code, status, copyright, total, name);
         return marvel;
 
